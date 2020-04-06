03d0b2cef897f76b6e9f392649f4138d7a53d27d
yuer2008 <yangfutao2000@163.com>
2020-04-06 21:56:00 +0800

    变量声明和赋值

---
+++ study/day01/1var/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+//变量的声明和赋值
+var (
+	name string
+	age  int
+)
+
+func main() {
+	var add string
+	var height = 100
+	width := 50
+	add = "shenzhen"
+	fmt.Printf("%s%d%d", add, height, width)
+}
